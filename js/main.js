9ac77fadc54bdc266fac928c8bbcacd417fa4211
Edward R <edwardrz@users.noreply.github.com>
2017-08-01 19:50:00 -0500

    added .js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
 console.log("main.js active");
 
 /***********  SCRIPT  ***************/
+let numIn1 = "";
+let numIn2 = "";
+let numOut1 = "";
+let numX = getElementById('numIn1');
+let numY = getElementById("numIn2");
 
 
-/***********  SCRIPT  ***************/
+object.addEventListener("click", myScript);
+object.addEventListener("click", myScript);
+object.addEventListener("click", myScript);
+object.addEventListener("click", myScript);
 
 
-/***********  SCRIPT  ***************/
+function calculate (x, y, func) {
+	return func(numX, numY)
+}
+
+function add(x,y) {
+	return x + y
+}
+
+function subtract(x, y) {
+  return x - y
+}
+
+function multipy(x, y) {
+  return x * y
+}
+function divide(x, y) {
+  return x / y
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
